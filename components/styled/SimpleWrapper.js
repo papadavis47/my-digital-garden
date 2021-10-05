baeaef56d78d1e4f66158bf2093f36105ef7269a
John William Davis <jwdavisdeveloper@gmail.com>
2021-10-04 22:31:21 -0700

    Add media query to simplewrapper component

--- components/styled/SimpleWrapper.js
+++ components/styled/SimpleWrapper.js
@@ -6,6 +6,10 @@ const SimpleWrapper = styled.div`
   align-items: center;
   justify-content: center;
   min-height: 100vh;
+
+  @media screen and (max-width: 600px) {
+    padding: 0 32px;
+  }
 `;
 
 export default SimpleWrapper;
