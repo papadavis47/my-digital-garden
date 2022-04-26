c4eb993b19e88251ab29de6ad9c679fe9f8f3553
John William Davis <jwdavisdeveloper@gmail.com>
2022-04-26 10:04:14 -0700

    Create clean slate for README, upgrade next and styled-components to @latest

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,15 @@
-import React from "react";
+import styled from "styled-components";
+
+const TitleWrapper = styled.h2`
+  color: blue;
+  font-style: italic;
+`;
 
 const Blog = () => {
   return (
     <div>
       <h1>This is the blog</h1>
+      <TitleWrapper>I am going to use MDX here.</TitleWrapper>
     </div>
   );
 };
